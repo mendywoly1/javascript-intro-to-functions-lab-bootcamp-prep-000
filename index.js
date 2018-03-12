0918baf9e6b831f240bfcbe36c9626ae2aaa83bc
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-12 23:18:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = string
-  if (lowercase.toLowerCase() === lowercase ){
+  if (string.toLowerCase() === string ){
     return "I can't hear you!"
   } else {
   return  "YES INDEED!"
